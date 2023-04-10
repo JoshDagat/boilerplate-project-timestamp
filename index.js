dd82f4b54b24e6443c5590e3e5ac567df33a086b
Josuer Bague <josuer.online.work@gmail.com>
2023-04-10 13:48:35 +0800

    Api sanity check

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date?', (req, req) => {
+  console.log("test");
+})
+
 
 
 // listen for requests :)
