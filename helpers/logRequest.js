d6c70888c65e9c58d453a548b2c59acd09bc4083
JoshDagat <josuer.online.work@gmail.com>
2023-06-10 22:36:30 +0800

    Full implementation of logRequest helper function

--- helpers/logRequest.js
+++ helpers/logRequest.js
@@ -1,6 +1,7 @@
 import {appendFile, mkdir} from 'node:fs';
 import * as path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import {format} from "date-fns";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -8,18 +9,19 @@ const __dirname = path.dirname(__filename);
 const requestLogsFilePath = path.resolve(__dirname, "..", "./logs/requestLogs.txt");
 
 export default function logRequest(req, res, next) {
+    const date = format(new Date(), 'MM/dd/yyyy pp')
+    const logText = `${req.method}\t${date}\t${req.url} ${req.headers.origin}\n`
 
-    appendFile(requestLogsFilePath, "test", (err) => {
+    appendFile(requestLogsFilePath, logText, (err) => {
         if (err) {
             
             const requestLogsDir = path.resolve(__dirname, "..", path.dirname(requestLogsFilePath));
-            console.log(requestLogsDir)
 
             mkdir(requestLogsDir, (err) => {
                 console.log(err);
             });
 
-            appendFile(requestLogsFilePath, "test-inner", (err) => console.log(err));
+            appendFile(requestLogsFilePath, logText, (err) => console.log(err));
         }
     })
     next()
